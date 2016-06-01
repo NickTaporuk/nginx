6197ef57cedb300099df1e0134e23c4b93c069b1
NickTaporuk <nictaporuk@mail.ru>
2016-06-01 21:05:28 +0300

    Update install_nginx_and_lua_rtmp_module.sh

--- install_nginx_and_lua_rtmp_module.sh
+++ install_nginx_and_lua_rtmp_module.sh
@@ -4,7 +4,7 @@ export LUAJIT_LIB=/usr/local/lib
 export LUAJIT_INC=/usr/local/include/luajit-2.1
 export LD_LIBRARY_PATH=/usr/local/lib/:$LD_LIBRARY_PATH
 
-./configure --prefix=/opt/nginx \
+./configure \
         --with-ld-opt="-Wl,-rpath,/usr/local/bin/luajit" \
         --add-module=/home/nick/nginx_compile/nginx-rtmp-module \
         --add-module=/home/nick/nginx_compile/ngx_devel_kit \
@@ -17,6 +17,42 @@ export LD_LIBRARY_PATH=/usr/local/lib/:$LD_LIBRARY_PATH
         --http-log-path=/var/log/nginx/access.log \
         --with-http_geoip_module \
         --with-http_stub_status_module
+        --prefix=/etc/nginx
+        --sbin-path=/usr/sbin/nginx
+        --conf-path=/etc/nginx/nginx.conf
+        --error-log-path=/var/log/nginx/error.log
+        --http-log-path=/var/log/nginx/access.log
+        --pid-path=/var/run/nginx.pid
+        --lock-path=/var/run/nginx.lock
+        --http-client-body-temp-path=/var/cache/nginx/client_temp
+        --http-proxy-temp-path=/var/cache/nginx/proxy_temp
+        --http-fastcgi-temp-path=/var/cache/nginx/fastcgi_temp
+        --http-uwsgi-temp-path=/var/cache/nginx/uwsgi_temp
+        --http-scgi-temp-path=/var/cache/nginx/scgi_temp
+        --user=nginx
+        --group=nginx
+        --with-http_ssl_module
+        --with-http_realip_module
+        --with-http_addition_module
+        --with-http_sub_module
+        --with-http_dav_module
+        --with-http_flv_module
+        --with-http_mp4_module
+        --with-http_gunzip_module
+        --with-http_gzip_static_module
+        --with-http_random_index_module
+        --with-http_secure_link_module
+        --with-http_stub_status_module
+        --with-http_auth_request_module
+        --with-threads
+        --with-stream
+        --with-stream_ssl_module
+        --with-http_slice_module
+        --with-mail
+        --with-mail_ssl_module
+        --with-file-aio
+        --with-http_v2_module
+        --with-ipv6
 
 make -j2
 sudo make install
